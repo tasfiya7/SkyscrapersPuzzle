8fede207fdc4968cf264cd61ffaec32857abe653
tasfiya7 <145740391+tasfiya7@users.noreply.github.com>
2025-02-24 05:51:59 +0000

    1 to 0

--- src/hw1_game.c
+++ src/hw1_game.c
@@ -8,12 +8,12 @@
 int main(int argc, char **argv) {
     if (argc != 4) {
         fprintf(stderr, "Usage: %s size initial_state keys\n", argv[0]);
-        return 1;
+        return 0;
     }
     int size = (int)strtoul(argv[1], NULL, 10);
     if (!initialize_board(argv[2], argv[3], size)) {
         printf("Invalid initial board state.\n");
-        return 1;
+        return 0;  // exit gracefully with 0
     }
     print_board(size);
     handle_user_input(size);
